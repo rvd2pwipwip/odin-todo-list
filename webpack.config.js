c45c50c83ca0bfc4bcf6507fc4b22c142c2f43b9
Herve Desrosiers <herve_desrosiers@hotmail.com>
2024-11-26 08:59:02 -0500

    webpack config to copy json in public instead of dist root

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
     }),
     new CopyWebpackPlugin({
       patterns: [
-        { from: './src/public', to: '' }
+        { from: 'public', to: 'public' }
       ],
     }),
   ],
